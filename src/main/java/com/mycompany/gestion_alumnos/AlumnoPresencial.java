6c4ac3c34f7c580c31b9c7975a2d9fb5eff13d2e
alons <alons@Alonso>
2026-03-14 22:31:14 -0600

    Commit Actividad 3

---
+++ src/main/java/com/mycompany/gestion_alumnos/AlumnoPresencial.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.gestion_alumnos;
+
+/**
+ *
+ * @author alons
+ */
+public class AlumnoPresencial extends Alumno{
+    private String aula;
+    private String edificio;
+    public AlumnoPresencial(String matricula, String nombre, int edad, String carrera, String aula, String edificio) {
+        super(matricula, nombre, edad, carrera);
+        this.aula = aula;
+        this.edificio = edificio;
+    }
+    
+    public void setAula(String aula) {
+        this.aula = aula;
+    }
+
+    public void setEdificio(String edificio) {
+        this.edificio = edificio;
+    }
+    
+    public String info(){
+        
+            return super.info() + "| Aula: " + aula +
+                   "| Edificio: " + edificio;
+    }
+}
+    
